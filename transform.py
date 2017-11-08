8c4dffe0ff9a5d3e56866e2d29c129426e5684af
Krishn Ramesh <krishnr@gmail.com>
2017-11-08 01:43:44 -0500

    building dataset

--- transform.py
+++ transform.py
@@ -1,6 +1,22 @@
 import csv
+import json
+
+summary_dataset = []
 
 with open('./OPP-115/annotations/105_amazon.com.csv') as amzn:
-    reader = csv.reader(amzn, dialect='excel')
+    reader = csv.reader(amzn)
     for row in reader:
-        print '\n\n'.join(row)
+        temp_dict = {}
+        annotation_id = row[0]
+        dimension = row[5]
+        attrs = json.loads(row[6])
+        raw_text = ''
+        for category in attrs:
+            if 'selectedText' in attrs[category]:
+                raw_text += ' ' + attrs[category]['selectedText']
+
+        temp_dict['annotation_id'] = annotation_id
+        temp_dict['dimension'] = dimension
+        temp_dict['raw_text'] = raw_text
+
+        summary_dataset.append(temp_dict)
