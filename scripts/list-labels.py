0afab328cde17d30e7c84e1ffd918d4af44b4910
Krishn Ramesh <krishnr@gmail.com>
2018-02-13 16:20:26 -0500

    added dimensions to label list

--- scripts/list-labels.py
+++ scripts/list-labels.py
@@ -1,18 +1,66 @@
 import os
 import csv
+import re
 
-directory = '../OPP-115/pretty_print_uniquified/'
+dim_map = {
+    'First Party Collection/Use': 'Collection',
+    'Third Party Sharing/Collection': 'Third Party',
+    'User Choice/Control': 'Choices',
+    'User Access, Edit and Deletion': 'Choices',
+    'Data Retention': 'Storage',
+    'Data Security': 'Storage', 
+    'Policy Change': 'Choices',
+    'Do Not Track': 'Do Not Track',
+    'International and Specific Audiences': 'International',
+    'Other': 'Other'
+}
 
-labels = []
+def build_dataset(a_file, l_file):
+    dataset_arr = []
+    with open(a_file) as annotation_file:
+        reader = csv.reader(annotation_file)
+        for row in reader:
+            temp_dict = {}
+            annotation_id = row[0]
+            opp_dimension = row[5]
 
-for filename in os.listdir(directory):
-    with open(directory + filename) as label_file:
+            temp_dict['annotation_id'] = annotation_id
+            dimension = dim_map[opp_dimension]
+            temp_dict['dimension'] = dimension
+
+            dataset_arr.append(temp_dict)
+
+    label_dataset = {}
+    with open(l_file) as label_file:
         reader = csv.reader(label_file)
         for row in reader:
-            labels.append(row[0])
+            annotation_id = row[0]
+            label = row[3]
+            label_dataset[annotation_id] = label
 
-labels = list(set(labels))
+    return_arr = []
+    
+    for row in dataset_arr:
+        row['label'] = label_dataset[row['annotation_id']]
+        return_arr.append((row['label'], row['dimension']))
 
-with open('../labels.csv', 'w') as f:
-    wr = csv.writer(f, delimiter='\n')
-    wr.writerow(labels)
+    return return_arr
+
+labels_list = []
+
+label_directory = '../OPP-115/pretty_print/'
+annotation_directory = '../OPP-115/annotations/'
+
+for filename in os.listdir(annotation_directory):
+    match = re.search('[\d]+_(.+)', filename)
+    label_name = match[1]
+    l_file = label_directory + label_name
+    a_file = annotation_directory + filename
+    labels_list += build_dataset(a_file, l_file)
+
+labels_list = list(set(labels_list))
+
+with open('../labels_list.csv', 'w') as f:
+    wr = csv.writer(f,delimiter=',')
+    for line in labels_list:
+        wr.writerow(line)
