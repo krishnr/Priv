3555cea4b5721bbd6660411ae555cf184a818945
Krishn Ramesh <krishnr@gmail.com>
2018-02-24 16:33:15 -0500

    added new labels

--- scripts/list-labels.py
+++ scripts/list-labels.py
@@ -2,16 +2,15 @@ import os
 import csv
 import re
 
-
-
+dim_list = []
 def get_dimension(label, dimension):
     dim_map = {
     'First Party Collection/Use': 'Collection',
     'Third Party Sharing/Collection': 'Third Party',
     'User Choice/Control': 'Choices',
     'User Access, Edit and Deletion': 'Choices',
-    'Data Retention': 'Storage',
-    'Data Security': 'Storage', 
+    'Data Retention': 'Retention',
+    'Data Security': 'Security', 
     'Policy Change': 'Choices',
     'Do Not Track': 'Do Not Track',
     'International and Specific Audiences': 'International',
@@ -20,8 +19,56 @@ def get_dimension(label, dimension):
     
     if 'location' in label.lower():
         return 'Location'
+    
+    if 'ip address' in label.lower():
+        return 'IP Address'
+    
+    if 'targeted advertising' in label.lower():
+        return 'Targeted Advertising'
+    
+    if any(s in label.lower() for s in ['change', 'update', 'notif']) or dimension == 'Policy Change':
+        return 'Policy Change'
 
-    return dim_map[dimension]
+    if dimension == 'Do Not Track':
+        return 'Do Not Track'
+    
+    if dimension == 'Data Security':
+        return 'Security'
+    
+    if any(s in label.lower() for s in ['opt out', 'link']):
+        return 'Action'
+
+    if 'financial' in label.lower():
+        return 'Payment'
+    
+    if any(s in label.lower() for s in ['legal', 'law']):
+        return 'Disclosure'
+    
+    if 'health' in label.lower():
+        return 'Health'
+    
+    if all(s in label.lower() for s in ['delete', 'account']):
+        return 'Deletion'
+    
+    if 'social media' in label.lower():
+        return 'Social Media'
+
+    if 'device id' in label.lower():
+        return 'Identification'
+    
+    if any(s in label.lower() for s in ['personalization', 'customization']):
+        return 'Personalization'
+    
+    if 'cookies' in label.lower():
+        return 'Cookies'
+
+    if any(s in label.lower() for s in ['demographic', 'analytics', 'research', 'identifiable']):
+        if 'third party' in label.lower():
+            return '3P Analysis'
+        else:
+            return '1P Analysis'
+
+    return None
 
 def build_dataset(a_file, l_file):
     dataset_arr = []
@@ -50,6 +97,9 @@ def build_dataset(a_file, l_file):
     for row in dataset_arr:
         row['label'] = label_dataset[row['annotation_id']]
         dimension = get_dimension(row['label'], row['dimension'])
+        if not dimension:
+            continue
+        dim_list.append(dimension)
         return_arr.append((row['label'], dimension))
 
     return return_arr
@@ -59,14 +109,19 @@ labels_list = []
 label_directory = '../OPP-115/pretty_print/'
 annotation_directory = '../OPP-115/annotations/'
 
+
 for filename in os.listdir(annotation_directory):
     match = re.search('[\d]+_(.+)', filename)
     label_name = match[1]
     l_file = label_directory + label_name
     a_file = annotation_directory + filename
-    labels_list += build_dataset(a_file, l_file)
+    label_tuple = build_dataset(a_file, l_file)
+    labels_list += label_tuple
 
 labels_list = list(set(labels_list))
+
+counts =  [(x, dim_list.count(x)) for x in set(dim_list)]
+print(counts)
 
 with open('../labels_list.csv', 'w') as f:
     wr = csv.writer(f,delimiter=',')
