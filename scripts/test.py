8b2ad878bbf5f64feef658564cde71d647c50068
Krishn Ramesh <krishnr@gmail.com>
2018-02-26 03:18:19 -0500

    added answer classifiers

--- scripts/test.py
+++ scripts/test.py
@@ -10,11 +10,21 @@ with open(os.path.join(curr_folder, '../datasets/X_test.p'), 'rb') as handle:
     X_test = pickle.load(handle)
 with open(os.path.join(curr_folder, '../datasets/y_test.p'), 'rb') as handle:
     y_test = pickle.load(handle)
+with open(os.path.join(curr_folder, '../datasets/dim_data.p'), 'rb') as handle:
+    dim_data = pickle.load(handle)
 
 dim_clf = joblib.load(os.path.join(curr_folder, '../pickles/dim_clf.pkl'))
+ans_clfs = joblib.load(os.path.join(curr_folder, '../pickles/ans_clfs.pkl'))
 
 score = dim_clf.score(X_test, y_test)
-print("Classifier accuracy: %.3f" % score)
+print("Dimension classifier accuracy: %.3f" % score)
+
+for dim in dim_data:
+    X_test = dim_data[dim]['X_test']
+    y_test = dim_data[dim]['y_test']
+    score = ans_clfs[dim].score(X_test, y_test)
+    print("Answer classifier accuracy for %s: %.3f" % (dim, score))
+
 
 # Print incorrect results
 # for i, x in enumerate(X_test):
