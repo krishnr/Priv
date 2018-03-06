b44cf1561f0c34a02c9619bacae31ed1961c2c8b
rfarmaha <ramandeep.f@gmail.com>
2018-03-06 18:18:51 -0500

    Fixed volume mounting

--- server/container-run.sh
+++ server/container-run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -d -it --rm -p 80:80 --name priv-server -v $PWD/src:/server/src -v $PWD/pickles:/server/pickles priv-server-image
+docker run -d -it --rm -p 80:80 --name priv-server -v $PWD/src:/app/src -v $PWD/pickles:/app/pickles priv-server-image
